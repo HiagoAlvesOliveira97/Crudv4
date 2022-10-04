f9a515098eba702c06c8be5fd2d7a3953f0b7b0b
Hiago Alves <hiagoao@gmail.com>
2022-10-04 11:28:20 -0300

    Adicionar arquivos de projeto.

---
+++ Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Crudv4.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-CM8F2OC\SQLEXPRESS;Database=Crudv4;Integrated Security=True");
+        }
+    }
+}
